03d1c3a82b3bfb0e9e21b222cdce7e575287d2d8
Lance Poehler <lancepoehler@gmail.com>
2016-11-29 11:15:22 -0800

    clean up actions

--- app/actions/airport_hvac_actions.jsx
+++ app/actions/airport_hvac_actions.jsx
@@ -2,14 +2,13 @@ const Dispatcher = require('../dispatchers/airport_hvac_dispatcher.jsx');
 const Constants = require('../constants/airport_hvac_constants.jsx');
 
 var AirportHvacActions = {
-  //Action Definition
 
-  getUpdatedAirportDate(startTime, endTime) {
-      Dispatcher.handleViewAction({
-        type:Constants.GET,
-        data: {startTime, endTime}
-    });
-  },
+    getUpdatedAirportDate(startTime, endTime) {
+        Dispatcher.handleViewAction({
+            type:Constants.GET,
+            data: {startTime, endTime}
+        });
+    },
 };
 
 module.exports = AirportHvacActions;
